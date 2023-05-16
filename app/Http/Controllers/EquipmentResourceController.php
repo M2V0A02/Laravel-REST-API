d403cd12c87917a4ba819bcf314a9584ce11f1d2
M2V0A02 <volodin.mixa2015@yandex.ru>
2023-05-16 18:15:32 +0500

    Сделал Store, show, update, destroy методы для Контроллера ресурсов

--- app/Http/Controllers/EquipmentResourceController.php
+++ app/Http/Controllers/EquipmentResourceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\EquipmentCollection;
+use App\Http\Resources\EquipmentResource;
 use Illuminate\Http\Request;
 use App\Models\Equipment;
 
@@ -16,23 +17,27 @@ class EquipmentResourceController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $equipment = new Equipment($request->all());
+        $equipment->save();
+        return new EquipmentResource($equipment);
     }
 
     
     public function show(string $id)
     {
-        //
+        return new EquipmentResource(Equipment::findOrFail($id));
     }
     
     public function update(Request $request, string $id)
     {
-        //
+        $equipment = Equipment::find($id);
+        $equipment->fill($request->all());
+        $equipment->save();
     }
 
     
     public function destroy(string $id)
     {
-        //
+        Equipment::destroy($id);
     }
 }
