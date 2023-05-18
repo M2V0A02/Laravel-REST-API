ed645c40711548c378abc017934537dd89d0dd6f
M2V0A02 <volodin.mixa2015@yandex.ru>
2023-05-18 18:57:04 +0500

    Сделал поле serial_number обязательным и уникальным

--- database/migrations/2023_05_16_061920_create_equipment_table.php
+++ database/migrations/2023_05_16_061920_create_equipment_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('equipment', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('equipment_type_id');
-            $table->string('serial_number');
+            $table->string('serial_number')->unique()->nullable(false);;
             $table->text('desc')->nullable();
             $table->timestamps();
             $table->softDeletes();
